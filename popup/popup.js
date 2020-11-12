45c11ca5405927eeb9affa961d54c3a93001e909
ZacLap <Zeusay170@gmail.com>
2020-11-12 13:53:33 -0500

    This is everything stripped down and ported to chrome

--- popup/popup.js
+++ popup/popup.js
@@ -1,3 +1,4 @@
+
 var
   popupButtonSettings, popupCounter, popupTextarea, popupTextareaContainer, popupFilterTabs, popupFilterTabsContainer,
   popupButtonCopy, popupButtonExport,
@@ -12,11 +13,11 @@ var defaultPopupStates = {
   }
 }
 
-browser.runtime.getPlatformInfo(function (info) {
+chrome.runtime.getPlatformInfo(function (info) {
   os = info.os
 })
 
-browser.windows.getLastFocused(function (currentWindow) {
+chrome.windows.getLastFocused(function (currentWindow) {
   currentWindowId = currentWindow.id
 })
 
@@ -33,20 +34,18 @@ w.addEventListener('load', function () {
   popupButtonCopy = d.getElementsByClassName('popup-button-copy')[0]
   popupButtonExport = d.getElementsByClassName('popup-button-export')[0]
   popupButtonSettings = d.getElementsByClassName('popup-button-settings')[0]
-
-  setLimitWindowVisibility()
+  
+  updatePopup()
 
   popupFormat.addEventListener('change', function () {
-    savePopupStates()
     updatePopup()
   })
 
   popupButtonSettings.addEventListener('click', function () {
-    browser.runtime.openOptionsPage()
+    chrome.runtime.openOptionsPage()
   })
 
   popupLimitWindow.addEventListener('change', function () {
-    savePopupStates()
     updatePopup()
   })
 
@@ -61,75 +60,39 @@ w.addEventListener('load', function () {
   popupButtonExport.addEventListener('click', function () {
     download()
   })
-
-  getOptions()
-  restorePopupStates()
-
-  localization()
 })
 
 function updatePopup () {
-  browser.tabs.query(
-    {},
+  chrome.tabs.query(
+    {currentWindow: true},
     function (tabs) {
       var list = ''
-      var header = ''
-      var format = '{url}\r\n'
       var actualNbTabs = 0
       var totalNbTabs = tabs.length
       var nbFilterMatch = 0
       var userInput = popupFilterTabs.value
 
-      if (popupFormat.checked) format = '{title}\r\n{url}\r\n\r\n'
-
-      if (optionsFormatCustom) {
-        popupLabelFormatTitles.classList.add('hidden')
-        popupLabelFormatCustom.classList.remove('hidden')
-
-        if (popupFormat.checked) format = optionsFormatCustom.replace(/\\n/g, '\n').replace(/\\r/g, '\r')
-      }
-
-      if (optionsFilterTabs) popupFilterTabsContainer.classList.remove('hidden')
-
       for (var i = 0; i < totalNbTabs; i++) {
-        var tabWindowId = tabs[i].windowId
-        var tabPinned = tabs[i].pinned
         var tabURL = tabs[i].url
-        var tabTitle = tabs[i].title
 
-        if (optionsIgnorePinned && tabPinned) continue
-        if (popupLimitWindow.checked && tabWindowId !== currentWindowId) continue
-
-        if ((optionsIgnoreNonHTTP && tabURL.startsWith('http')) || !optionsIgnoreNonHTTP) {
+        if (tabURL.startsWith('http')){
           actualNbTabs += 1
-
-          if (filterMatch(userInput, [tabTitle, tabURL]) || userInput === '') {
+          if (filterMatch(userInput, [ tabURL]) || userInput === '') {
             nbFilterMatch += 1
-
-            if (/<\/?[a-zA-Z]+\/?>/.test(format)) tabTitle = tabTitle.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;')
-
-            list += format.replace(/{title}/g, tabTitle).replace(/{url}/g, tabURL).replace(/{window-id}/g, tabWindowId)
+            list += tabURL + '\r\n'
           }
         }
-      }
 
       popupTextarea.value = ''
-
-      if (optionsCustomHeader) {
-        var nbTabs = (userInput !== '') ? nbFilterMatch : actualNbTabs
-
-        header = optionsCustomHeader.replace(/\\n/g, '\n').replace(/\\r/g, '\r').replace(/{num-tabs}/g, nbTabs)
-
-        popupTextarea.value += header + '\r\n\r\n'
-      }
 
       popupTextarea.value += list
       popupCounter.textContent = (userInput !== '') ? nbFilterMatch + ' / ' + actualNbTabs : actualNbTabs
 
       setSeparatorStyle()
       popupFilterTabs.focus()
+    
     }
-  )
+  })
 }
 
 function filterMatch (needle, haystack) {
@@ -151,15 +114,6 @@ function setSeparatorStyle () {
   }
 }
 
-function setLimitWindowVisibility () {
-  let getting = browser.windows.getAll()
-
-  getting.then(function (windowInfoArray) {
-    if (windowInfoArray.length > 1) {
-      popupLimitWindow.parentNode.classList.remove('hidden')
-    }
-  })
-}
 
 function copyToClipboard () {
   if (popupButtonCopy.classList.contains('disabled')) return
@@ -168,17 +122,17 @@ function copyToClipboard () {
 
   var message = d.execCommand('copy') ? 'copiedToClipboard' : 'notCopiedToClipboard'
 
-  browser.notifications.create('ExportTabsURLs', {
+  chrome.notifications.create('ExportTabsURLs', {
     'type': 'basic',
-    'title': browser.i18n.getMessage('appName'),
+    'title': chrome.i18n.getMessage('appName'),
     'iconUrl': '../img/icon.svg',
-    'message': browser.i18n.getMessage(message)
+    'message': chrome.i18n.getMessage(message)
   })
 
   popupButtonCopy.classList.add('disabled')
 
   setTimeout(function () {
-    browser.notifications.clear('ExportTabsURLs')
+    chrome.notifications.clear('ExportTabsURLs')
     popupButtonCopy.classList.remove('disabled')
   }, 3000)
 }
@@ -197,36 +151,4 @@ function download () {
   d.body.appendChild(element)
   element.click()
   d.body.removeChild(element)
-}
-
-function restorePopupStates () {
-  let gettingItem = browser.storage.local.get(defaultPopupStates)
-
-  gettingItem.then(function (items) {
-    popupLimitWindow.checked = items.states.popupLimitWindow
-    popupFormat.checked = items.states.format
-
-    updatePopup()
-  })
-}
-
-function savePopupStates () {
-  browser.storage.local.set({
-    'states': {
-      format: popupFormat.checked,
-      popupLimitWindow: popupLimitWindow.checked
-    }
-  })
-}
-
-function getOptions () {
-  let gettingItem = browser.storage.local.get(defaultOptions)
-
-  gettingItem.then(function (items) {
-    optionsIgnoreNonHTTP = items.options.ignoreNonHTTP
-    optionsIgnorePinned = items.options.ignorePinned
-    optionsFormatCustom = items.options.formatCustom
-    optionsFilterTabs = items.options.filterTabs
-    optionsCustomHeader = items.options.customHeader
-  })
 }
